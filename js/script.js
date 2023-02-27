cae8eb0bdb7665f62afba05b81020070a78f91c7
Sabir-itobuz <sabir@itobuz.com>
2023-02-27 18:45:02 +0530

    nothing

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    centeredSlides: true,
+    freeMode: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints :{
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        }
+    }
+});
+
